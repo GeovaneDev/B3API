1b450716070338f32f468a0714de3cbb71cadab7
Geovane <87013843+GeovaneDev@users.noreply.github.com>
2024-01-28 10:36:28 -0300

    Adicionado headers na api de fundamentus

--- pages/api/fundamentus/available.js
+++ pages/api/fundamentus/available.js
@@ -3,9 +3,18 @@ import iconv from 'iconv-lite';
 
 export default async function handler(req, res) {
   try {
-    const response = await axios.post('https://www.fundamentus.com.br/script/cmplte.php', {
-      responseType: 'arraybuffer',
-    });
+    const response = await axios.post(
+      `https://www.fundamentus.com.br/script/cmplte.php`,
+      {},
+      {
+          responseType: 'arraybuffer',
+          headers: {
+              'User-agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201',
+              'Accept': 'text/html, text/plain, text/css, text/sgml, */*;q=0.01',
+              'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+          },
+      }
+  );
 
     const html = iconv.decode(response.data, 'iso-8859-1');
     const match = html.match(/var tokens = (\[.*?\]);/s);
