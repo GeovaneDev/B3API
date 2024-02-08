cec415d2cfc38b1adc8de9b50957e9376a364c57
Geovane <87013843+GeovaneDev@users.noreply.github.com>
2024-02-08 18:01:11 -0300

    Pequena mudança no titulo

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'BrInvestAPI - Dados Financeiros Brasileiros em Tempo Real | API B3',
+  title: 'BrInvestAPI - Dados Financeiros Brasileiros | API B3',
   description: 'Descubra a melhor API B3, BrInvestAPI. Fornece dados confiáveis e em tempo real sobre ações, cotações, índices e informações essenciais para o mercado financeiro do Brasil. Experimente agora!'
 }
 
